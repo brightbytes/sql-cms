2bb4958fee836a6e444113342c18abb51875c622
Aaron Cohen <aaron@brightbytes.net>
2017-02-08 22:06:24 -0800

    Remove dpp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,6 @@ module ApplicationHelper
   # JSON stuff
 
   def pretty_print_as_json(json)
-    dpp JSON.pretty_generate(json.as_json)
     JSON.pretty_generate(json.as_json).gsub(/\"(.+)\":/, '\1:').gsub(" ", "&nbsp;").gsub("\n", "<br />").html_safe if json.present?
   end
 
