48c64e779b2b12573f2e005f565f2bd4c50dd98c
Aaron Cohen <aaron@brightbytes.net>
2017-04-04 16:03:36 -0700

    Improve coverage mildly

--- spec/support/concern_shared_examples.rb
+++ spec/support/concern_shared_examples.rb
@@ -16,19 +16,34 @@ shared_examples 'yaml helper methods' do
     expect(subject).to_not be_valid
   end
 
+  # This test is extraordinarily silly (i.e. quite pathetic) - it should really be separate specs in the parent files.  Bah.
   it "should provide instance- and class-level interpolation methods" do
     subject.params = { foo: :bar, whatever: :dude }
+    name = "Silly :foo, :whatever, dude"
     sql = "SELECT :foo FROM :whatever"
     if subject.is_a?(TransformValidation)
       subject.validation.sql = sql
+      subject.validation.name = name
     elsif subject.is_a?(WorkflowDataQualityReport)
       subject.data_quality_report.sql = sql
+      subject.data_quality_report.name = name
     else
       subject.sql = sql
+      subject.name = name
     end
     expect(subject.interpolated_sql).to eq("SELECT bar FROM dude")
+    expect(subject.interpolated_name).to eq("Silly bar, dude, dude")
 
     expect(subject.class.interpolate(sql: sql, params: subject.params)).to eq("SELECT bar FROM dude")
+
+    # Transforms are the only subject where params can be empty
+    if subject.is_a?(Transform)
+      subject.params = {}
+      expect(subject.interpolated_sql).to eq(sql)
+      expect(subject.interpolated_name).to eq(name)
+    end
+
+
   end
 
 end
