77e358c9cf6f766a5fc05a9e869720cc74729f5b
Aaron Cohen <aaron@brightbytes.net>
2017-05-23 14:55:20 -0700

    Fix wrapping of JSON fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,13 +37,13 @@ module ApplicationHelper
   # JSON stuff
 
   def pretty_print_as_json(json)
-    JSON.pretty_generate(json.as_json).gsub(/\"(.+)\":/, '\1:').gsub(" ", "&nbsp;").gsub("\n", "<br />").html_safe if json.present?
+    JSON.pretty_generate(json.as_json).gsub(/\"(.+)\":/, '\1:').gsub("\n", "<br />").html_safe if json.present?
   end
 
   # Kinda lame, but whatever
-  def pretty_print_json(json)
-    pretty_print_as_json(JSON.parse(json))
-  end
+  # def pretty_print_json(json)
+  #   pretty_print_as_json(JSON.parse(json))
+  # end
 
   def sql_newlines_to_array(hash)
     hash = hash.with_indifferent_access
