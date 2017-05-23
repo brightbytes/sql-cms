3b542fc902a3f956083a8991807fb557876fb88b
Aaron Cohen <aaron@brightbytes.net>
2017-05-23 15:41:19 -0700

    UI tweak

--- app/admin/run_step_log.rb
+++ app/admin/run_step_log.rb
@@ -15,18 +15,20 @@ ActiveAdmin.register RunStepLog do
       row :step_id
       row(:plan_source_step) do
         text_node(auto_link(run_step_log.plan_source_step))
-        # FIXME - Add Edit Link Here
+        text_node("&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;".html_safe)
+        meth = (run_step_log.transform_log? ? :edit_transform_path : :edit_workflow_data_quality_report_path)
+        text_node(link_to("EDIT", send(meth, run_step_log.plan_source_step)))
       end
 
       row(:human_status) { human_status(resource) }
 
       row(:duration) { Time.at(resource.duration_seconds).getutc.strftime("%H:%M:%S") }
 
-      row(:step_plan) { code(pretty_print_as_json(resource.step_plan)) }
-
       row(:step_result) { code(pretty_print_as_json(resource.step_result)) } if resource.step_result.present?
       row(:step_validation_failures) { code(pretty_print_as_json(resource.step_validation_failures)) } if resource.step_validation_failures.present?
       row(:step_exceptions) { code(pretty_print_as_json(resource.step_exceptions)) } if resource.step_exceptions.present?
+
+      row(:step_plan) { code(pretty_print_as_json(resource.step_plan)) }
 
       row :created_at
       row :updated_at
