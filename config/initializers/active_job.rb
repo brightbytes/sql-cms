f8790f8a8b1c001f886bd1d469a7121031a4e836
Aaron Cohen <aaron@brightbytes.net>
2017-05-25 17:33:05 -0700

    Reduce sidekiq polling interval

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -1,5 +1,5 @@
 ActiveJob::Base.queue_adapter = :sidekiq
 Sidekiq.default_worker_options = { backtrace: true, retry: 3 } # We only have retries for the case where we exhaust the connection pool
 Sidekiq.configure_server do |config|
-  config.average_scheduled_poll_interval = 3
+  config.average_scheduled_poll_interval = 2
 end
